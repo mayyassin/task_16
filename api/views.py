647380dc210b70eefd80b0b5c705d484ebd5451c
your-username <your-email>
2018-10-16 16:48:39 +0300

    task submission

--- api/views.py
+++ api/views.py
@@ -1,41 +1,46 @@
 from restaurants.models import Restaurant
 from rest_framework.generics import (
-    ListAPIView,
-    RetrieveAPIView,
-    RetrieveUpdateAPIView,
-    DestroyAPIView,
+	ListAPIView,
+	RetrieveAPIView,
+	RetrieveUpdateAPIView,
+	DestroyAPIView,
+	CreateAPIView,
 )
 from .serializers import (
-    RestaurantListSerializer,
-    RestaurantDetailSerializer,
-    RestaurantCreateUpdateSerializer,
+	RestaurantListSerializer,
+	RestaurantDetailSerializer,
+	RestaurantCreateUpdateSerializer,
 )
 
 class RestaurantListView(ListAPIView):
-    queryset = Restaurant.objects.all()
-    serializer_class = RestaurantListSerializer
+	queryset = Restaurant.objects.all()
+	serializer_class = RestaurantListSerializer
 
 
 class RestaurantDetailView(RetrieveAPIView):
-    queryset = Restaurant.objects.all()
-    serializer_class = RestaurantDetailSerializer
-    lookup_field = 'id'
-    lookup_url_kwarg = 'restaurant_id'
+	queryset = Restaurant.objects.all()
+	serializer_class = RestaurantDetailSerializer
+	lookup_field = 'id'
+	lookup_url_kwarg = 'restaurant_id'
 
 
 # Complete Me
-class RestaurantCreateView():
+class RestaurantCreateView(CreateAPIView):
+	serializer_class = RestaurantCreateUpdateSerializer
 
+	def perform_create(self, serializer):
+		serializer.save(owner=self.request.user)
 
 class RestaurantUpdateView(RetrieveUpdateAPIView):
-    queryset = Restaurant.objects.all()
-    serializer_class = RestaurantCreateUpdateSerializer
-    lookup_field = 'id'
-    lookup_url_kwarg = 'restaurant_id'
+	queryset = Restaurant.objects.all()
+	serializer_class = RestaurantCreateUpdateSerializer
+	lookup_field = 'id'
+	lookup_url_kwarg = 'restaurant_id'
 
 
 class RestaurantDeleteView(DestroyAPIView):
-    queryset = Restaurant.objects.all()
-    serializer_class = RestaurantListSerializer
-    lookup_field = 'id'
-    lookup_url_kwarg = 'restaurant_id'
+	queryset = Restaurant.objects.all()
+	serializer_class = RestaurantListSerializer
+	lookup_field = 'id'
+	lookup_url_kwarg = 'restaurant_id'
+
